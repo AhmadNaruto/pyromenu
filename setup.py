4e5fd1fac086a9599e705e3a9a7b9ec3e6703875
IlhomBahoraliev <bahoralievdev@yandex.com>
2020-04-28 16:52:10 +0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def README():
+    with open("README.md") as file:
+        return file.read()
+
+
+def requirements():
+    with open("requirements.txt") as file:
+        return file.read().split("\n")
+
+
+setup(
+    name="pyromenu",
+    version="0.8",
+    author="LidaRandom",
+    author_email="bahoralievdev@yandex.com",
+    description="Object-oriented way to build telegram keyboard-menus",
+    long_description=README(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/IlhomBahoraliev/pyromenu",
+    packages=["pyromenu"],
+    install_requires=requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
