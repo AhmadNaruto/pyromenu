2680e1aae4535d949426b6fc9a4907a5482190af
IlhomBahoraliev <bahoralievdev@yandex.com>
2020-04-28 15:18:17 +0500

    create KMenu class, which implement Menu interface

---
+++ pyromenu/kmenu.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from pyrogram import ReplyKeyboardMarkup
+
+from interfaces import Menu, Row
+
+
+class KMenu(Menu):
+    """Keyboard menu
+    
+       Args:
+        rows: `Row` - rows of keyboard menu
+
+    """
+
+    def __init__(self, *rows: Row):
+        self._rows: List[Row] = list(rows)
+
+    def keyboard(self) -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(
+            [row.keyboard_row() for row in self._rows], resize_keyboard=True
+        )
+
+    def one_time_keyboard(self) -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(
+            [row.keyboard_row() for row in self._rows],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
